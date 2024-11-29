891476c8f713655912f0e2dac8323d76cd03e5c1
Sankit Saini <sainisankit5@gmail.com>
2024-11-29 09:13:52 +0530

    Ui changes for albums_page and album_widget

--- lib/presentation/pages/albums_page.dart
+++ lib/presentation/pages/albums_page.dart
@@ -1,4 +1,9 @@
+import 'package:albums_app/bloc/albums_bloc/album_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/models/album.dart';
+import '../widgets/album_widget.dart';
 
 class AlbumsPage extends StatefulWidget {
   const AlbumsPage({super.key});
@@ -9,9 +14,41 @@ class AlbumsPage extends StatefulWidget {
 
 class _AlbumsPageState extends State<AlbumsPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<AlbumBloc>().add(AlbumFetched());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Albums App'),
+    return Scaffold(
+      body: BlocBuilder<AlbumBloc, AlbumState>(builder: (context, state) {
+        if (state is AlbumLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is AlbumLoaded) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 20),
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                Album album = state.albums[index % 4];
+                double deviceHeight = MediaQuery.of(context).size.height;
+                double rowHeight = (deviceHeight - 80) / 4;
+                return AlbumWidget(
+                  height: rowHeight,
+                  album: album,
+                );
+              },
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text('Error loading albums'),
+          );
+        }
+      }),
     );
   }
 }
---
+++ lib/presentation/widgets/album_widget.dart
@@ -0,0 +1,86 @@
+import 'package:albums_app/bloc/photos_bloc/photos_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/models/album.dart';
+import '../../data/models/photo.dart';
+
+class AlbumWidget extends StatefulWidget {
+  final double height;
+  final Album album;
+  const AlbumWidget({super.key, required this.height, required this.album});
+
+  @override
+  State<AlbumWidget> createState() => _AlbumWidgetState();
+}
+
+class _AlbumWidgetState extends State<AlbumWidget> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<PhotoBloc>().add(PhotosFetched(albumId: '${widget.album.id}'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: widget.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(widget.album.title),
+            SizedBox(
+              height: widget.height * 0.60,
+              child: BlocBuilder<PhotoBloc, PhotosState>(
+                  builder: (context, state) {
+                if (state is PhotosLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is PhotosLoaded) {
+                  int albumId = widget.album.id;
+                  if (!state.imagesByAlbum.containsKey('$albumId')) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      double screenWidth = MediaQuery.of(context).size.width;
+                      double itemWidth = (screenWidth - 60) / 3;
+
+                      List<Photo> images = state.imagesByAlbum['$albumId']!;
+                      Photo image = images[index % 3];
+
+                      return Image.network(
+                        image.url,
+                        width: itemWidth,
+                        height: 100,
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        width: 20,
+                      );
+                    },
+                    itemCount:
+                        1000, //given high value to imitate infinite scroll
+                  );
+                } else {
+                  return const Center(
+                    child: Text("Error loading images"),
+                  );
+                }
+              }),
+            ),
+            const Divider(
+              thickness: 2,
+              color: Colors.grey,
+            )
+          ],
+        ));
+  }
+}
